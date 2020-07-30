f62e84c01e6ee37709897800e4e5e181643a8292
Abdoulaye-Thespy <njigouhrazak@iut-dhaka.edu>
2020-07-30 10:50:02 +0100

    setup user login and sign up

--- db/schema.rb
+++ db/schema.rb
@@ -16,6 +16,8 @@ ActiveRecord::Schema.define(version: 2020_07_28_123013) do
   enable_extension "plpgsql"
 
   create_table "users", force: :cascade do |t|
+    t.string "username"
+    t.string "fullname"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
