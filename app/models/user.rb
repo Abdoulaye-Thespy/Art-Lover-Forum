000991f51d6626ce371a103c58efb4b1d3ab7b34
Abdoulaye-Thespy <njigouhrazak@iut-dhaka.edu>
2020-08-04 19:23:57 +0100

    fixed linters errors

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,14 +1,15 @@
-# rubocop:disable Metrics/LineLength
 class User < ApplicationRecord
   validates :username, :fullname, presence: true
   validates :username, uniqueness: true
-
+  # rubocop:disable Metrics/LineLength
   has_attached_file :image, styles: { medium: '300x300>', thumb: '100x100>' }, default_url: '/images/:style/missing.png'
   has_attached_file :coverimage, styles: { medium: '300x300>', thumb: '100x100>' }, default_url: '/images/:style/missing.png'
+  # rubocop:enable Metrics/LineLength
   validates_attachment_content_type :coverimage, content_type: %r{\Aimage/.*\z}
   has_many :hints, dependent: :destroy
   has_many :active_relationships, class_name: 'Follower', foreign_key: 'follower_id', dependent: :destroy
   has_many :passive_relationships, class_name: 'Follower', foreign_key: 'followed_id', dependent: :destroy
+
   has_many :following, through: :active_relationships, source: :followed
   has_many :followers, through: :passive_relationships, source: :follower
 
@@ -16,4 +17,3 @@ class User < ApplicationRecord
     hints.all
   end
 end
-# rubocop:enable Metrics/LineLength
