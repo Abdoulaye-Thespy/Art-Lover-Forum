7649d5cbc9319fd6956216cf8d2e59d7a0e064ee
Abdoulaye-Thespy <njigouhrazak@iut-dhaka.edu>
2020-08-06 08:44:21 +0100

    deploy

--- db/schema.rb
+++ db/schema.rb
@@ -10,42 +10,43 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20_200_802_105_404) do
+ActiveRecord::Schema.define(version: 2020_08_02_105404) do
+
   # These are extensions that must be enabled in order to support this database
-  enable_extension 'plpgsql'
+  enable_extension "plpgsql"
 
-  create_table 'followers', force: :cascade do |t|
-    t.integer 'follower_id'
-    t.integer 'followed_id'
-    t.datetime 'created_at', precision: 6, null: false
-    t.datetime 'updated_at', precision: 6, null: false
-    t.index ['followed_id'], name: 'index_followers_on_followed_id'
-    t.index %w[follower_id followed_id], name: 'index_followers_on_follower_id_and_followed_id', unique: true
-    t.index ['follower_id'], name: 'index_followers_on_follower_id'
+  create_table "followers", force: :cascade do |t|
+    t.integer "follower_id"
+    t.integer "followed_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["followed_id"], name: "index_followers_on_followed_id"
+    t.index ["follower_id", "followed_id"], name: "index_followers_on_follower_id_and_followed_id", unique: true
+    t.index ["follower_id"], name: "index_followers_on_follower_id"
   end
 
-  create_table 'hints', force: :cascade do |t|
-    t.string 'text'
-    t.bigint 'user_id'
-    t.datetime 'created_at', precision: 6, null: false
-    t.datetime 'updated_at', precision: 6, null: false
-    t.index ['user_id'], name: 'index_hints_on_user_id'
+  create_table "hints", force: :cascade do |t|
+    t.string "text"
+    t.bigint "user_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_hints_on_user_id"
   end
 
-  create_table 'users', force: :cascade do |t|
-    t.string 'username'
-    t.string 'fullname'
-    t.datetime 'created_at', precision: 6, null: false
-    t.datetime 'updated_at', precision: 6, null: false
-    t.string 'image_file_name'
-    t.string 'image_content_type'
-    t.integer 'image_file_size'
-    t.datetime 'image_updated_at'
-    t.string 'coverimage_file_name'
-    t.string 'coverimage_content_type'
-    t.integer 'coverimage_file_size'
-    t.datetime 'coverimage_updated_at'
+  create_table "users", force: :cascade do |t|
+    t.string "username"
+    t.string "fullname"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.string "image_file_name"
+    t.string "image_content_type"
+    t.integer "image_file_size"
+    t.datetime "image_updated_at"
+    t.string "coverimage_file_name"
+    t.string "coverimage_content_type"
+    t.integer "coverimage_file_size"
+    t.datetime "coverimage_updated_at"
   end
 
-  add_foreign_key 'hints', 'users'
+  add_foreign_key "hints", "users"
 end
